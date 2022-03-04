80c1d1e1575f68f9ce271e706fd35782b622d6d7
7399 <1910674037@qq.com>
2022-03-04 21:52:13 +0800

    response

---
+++ src/main/java/com/du/feheadstudio/response/ErrorType.java
@@ -0,0 +1,11 @@
+package com.du.feheadstudio.response;
+
+/**
+ * @Author DU425
+ * @Date 2022/1/13 9:38
+ * @Version 1.0
+ */
+public interface ErrorType {
+    long getCode();
+    String getMessage();
+}
